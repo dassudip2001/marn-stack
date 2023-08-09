cbbf88b9161c8da3498828701c127a9dd3f32215
Sudip Das <85440350+dassudip2001@users.noreply.github.com>
2023-08-09 11:43:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const title = <h1>hello world</h1>;
+
+const HeaderComonent = () => {
+  return <>hello world</>;
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<HeaderComonent />);
